1d078f285b727189ebb4d535eb0c846c3762cb99
moshfiqrony <rony@worklife.so>
2021-12-09 15:14:34 +0600

    MR | File listing script

---
+++ script.py
@@ -0,0 +1,18 @@
+import os
+
+# get all icon name
+my_file_list = os.listdir('.')
+
+# open readme file to write
+my_txt = open('README.md', 'w')
+
+# Write the title
+my_txt.write("# google-official-svg-icons\n")
+
+for file in my_file_list:
+    if '.svg' not in file:
+        continue
+    str = '!['+file+']('+file+')\n'
+    my_txt.write(str)
+
+my_txt.close()
